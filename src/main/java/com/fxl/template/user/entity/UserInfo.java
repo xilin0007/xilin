6aa3892a8d005b2b8a2815ebb90711ebbf529f6e
xilin0007 <fangxlsmile@163.com>
2017-03-25 21:07:32 +0800

    第二次公司提交lock相关

--- src/main/java/com/fxl/template/user/entity/UserInfo.java
+++ src/main/java/com/fxl/template/user/entity/UserInfo.java
@@ -6,7 +6,7 @@ import com.fxl.frame.base.BaseEntity;
 
 public class UserInfo extends BaseEntity {
 	
-	private static final long serialVersionUID = 111L;
+	private static final long serialVersionUID = 1L;
 
     private String mobile;
 
