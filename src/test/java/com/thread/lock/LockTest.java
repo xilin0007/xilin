31f8183d59caa25432b261693e6e592aae9fdc2e
xilin0007 <fangxlsmile@163.com>
2017-03-26 22:57:34 +0800

    jiali提交lock

--- src/test/java/com/thread/lock/LockTest.java
+++ src/test/java/com/thread/lock/LockTest.java
@@ -1,4 +1,9 @@
-package com;
+package com.thread.lock;
+
+import java.util.List;
+
+
+
 
 public class LockTest {
 	public static void main(String[] args) {
@@ -6,3 +11,76 @@ public class LockTest {
 		System.out.println(property);
 	}
 }
+
+//银行账户类
+class Account {
+	private double balance;
+
+	public double getBalance() {
+		return balance;
+	}
+
+	public void setBalance(double balance) {
+		this.balance = balance;
+	}
+	
+	//存钱
+	public synchronized void addAmount(double amount) {
+		try {
+			Thread.sleep(10);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		this.balance += amount;
+	}
+	
+	//取钱
+	public synchronized void substractAmount(double amount) {
+		try {
+			Thread.sleep(10);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		this.balance -= amount;
+	}
+	
+}
+
+//ATM线程银行类，调用取钱
+class Bank implements Runnable {
+	
+	private final Account account;
+	
+	public Bank(Account account) {
+		this.account = account;
+	}
+
+	@Override
+	public void run() {
+		try
+	}
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
