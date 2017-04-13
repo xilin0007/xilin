bdbaa0f59a33f24ea8498f31cce144e5b180b91b
xilin0007 <fangxlsmile@163.com>
2017-04-13 11:49:23 +0800

    引用传递test

--- src/test/java/com/Test.java
+++ src/test/java/com/Test.java
@@ -1,6 +1,8 @@
 package com;
 
-import com.enums.ConsultStatusEnums;
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class Test {
 	public static void main(String[] args) {
@@ -11,6 +13,55 @@ public class Test {
 		int i = 0;
 		System.out.println(ss[i++]);*/
 		
-		System.out.println(ConsultStatusEnums.getStatusName(5));
+//		System.out.println(ConsultStatusEnums.getStatusName(5));
+		
+		List<VoTest> list = new ArrayList<VoTest>();
+		List<Integer> ids = new ArrayList<Integer>(); 
+		VoTest vo1 = new VoTest(1, "aaa");
+		VoTest vo2 = new VoTest(2, "bbb");
+		VoTest vo3 = new VoTest(3, "ccc");
+		list.add(vo1);list.add(vo2);list.add(vo3);
+		
+		List<VoTest> listTemp = new ArrayList<VoTest>();
+		VoTest votest = new VoTest();
+		int a = 0;
+		for (VoTest vo : list) {
+//			votest.setId(vo.getId());
+//			votest.setName(vo.getName());
+			votest = vo;
+			listTemp.add(votest);
+//			ids.add(votest.getId());
+			a = vo.getId();
+			ids.add(a);
+		}
+		System.out.println(listTemp.toString());
+		System.out.println(ids.toString());
+	}
+}
+
+class VoTest {
+	private int id;
+	private String name;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public VoTest() {
+	}
+	public VoTest(int id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+	@Override
+	public String toString() {
+		return "VoTest [id=" + id + ", name=" + name + "]";
 	}
 }
