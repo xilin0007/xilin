069df02592ce2461bf6a9d7900057560d12f2aae
方熙林 <fangxl@91160.com>
2020-09-03 18:41:30 +0800

    测试ThreadPoolExecutor效率问题

--- src/test/java/com/thread/executor/poolTest/EfficiencyTest.java
+++ src/test/java/com/thread/executor/poolTest/EfficiencyTest.java
@@ -1,6 +1,8 @@
 package com.thread.executor.poolTest;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.*;
 
@@ -12,22 +14,38 @@ import java.util.concurrent.*;
  * @Copyright: 深圳市宁远科技股份有限公司版权所有(C)2019
  */
 public class EfficiencyTest {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         long start = System.currentTimeMillis();
-        ThreadPoolExecutor pool = new ThreadPoolExecutor(2, 4, 1000, TimeUnit.MILLISECONDS, new LinkedBlockingDeque<Runnable>());
+        /**
+         * 参数maximumPoolSize：
+         * ArrayBlockingQueue<Runnable>为指定大小存放任务的队列,
+         * 如果队列中任务已满，并且当前线程个数小于maximumPoolSize，那么会创建新的线程来执行任务。
+         * LinkedBlockingQueue<Runnable>该队列无大小限制，所以maximumPoolSize参数一般不生效
+         */
+        ThreadPoolExecutor pool = new ThreadPoolExecutor(3, 3, 10000, TimeUnit.MILLISECONDS, new LinkedBlockingDeque<Runnable>());
+        List<Future<String>> results = new ArrayList<>();
         for (int i = 0; i < 10; i++) {
+            final int ii = i;
             Future<String> submit = pool.submit(new Callable<String>() {
                 @Override
                 public String call() throws Exception {
+                    //System.out.println("执行线程-" + ii);
+                    System.out.println("线程名称："+Thread.currentThread().getName());
                     //休眠2s
-                    TimeUnit.SECONDS.sleep(2);
+                    TimeUnit.SECONDS.sleep(10);
                     return new Date().toString();
                 }
             });
+            results.add(submit);
         }
         //完成executor的执行
         pool.shutdown();
         long end = System.currentTimeMillis();
-        System.out.println(start - end);
+        //System.out.println(results.size());
+        //System.out.println(end - start);
+        /*for (int i = 0; i < results.size(); i++) {
+            Future<String> ret = results.get(i);
+            System.out.println("ret" + i + ": " + ret.get());
+        }*/
     }
 }
