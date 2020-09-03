10228b3fa4df3b619926f0da789a497ce2ee2376
方熙林 <fangxl@91160.com>
2020-09-03 18:45:40 +0800

    设置HttpClient支持https接口的调用

--- src/main/java/com/fxl/frame/util/http/HttpClientUtil.java
+++ src/main/java/com/fxl/frame/util/http/HttpClientUtil.java
@@ -1,16 +1,5 @@
 package com.fxl.frame.util.http;
 
-import java.io.File;
-import java.io.IOException;
-import java.security.KeyManagementException;
-import java.security.KeyStoreException;
-import java.security.NoSuchAlgorithmException;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Map;
-import java.util.Map.Entry;
-
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpStatus;
 import org.apache.http.client.config.RequestConfig;
@@ -20,10 +9,11 @@ import org.apache.http.client.methods.HttpPost;
 import org.apache.http.config.Registry;
 import org.apache.http.config.RegistryBuilder;
 import org.apache.http.conn.socket.ConnectionSocketFactory;
+import org.apache.http.conn.socket.LayeredConnectionSocketFactory;
 import org.apache.http.conn.socket.PlainConnectionSocketFactory;
+import org.apache.http.conn.ssl.NoopHostnameVerifier;
 import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
-import org.apache.http.conn.ssl.SSLContextBuilder;
-import org.apache.http.conn.ssl.TrustSelfSignedStrategy;
+import org.apache.http.conn.ssl.TrustStrategy;
 import org.apache.http.entity.ContentType;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.entity.mime.MultipartEntityBuilder;
@@ -33,10 +23,25 @@ import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.DefaultHttpRequestRetryHandler;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
+import org.apache.http.ssl.SSLContexts;
 import org.apache.http.util.EntityUtils;
 
+import javax.net.ssl.SSLContext;
+import java.io.File;
+import java.io.IOException;
+import java.security.KeyManagementException;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+
 /**
- * apache httpClient
+ * apache httpClient，支持https的调用
  * @author fangxilin
  * @date 2018年2月1日
  */
@@ -60,25 +65,36 @@ public class HttpClientUtil {
     // 请求配置
     private static RequestConfig requestConfig;
 
+    private static CloseableHttpClient httpClient;
+
     static {
         
         try {
             //System.out.println("初始化HttpClientTest~~~开始");
-            SSLContextBuilder builder = new SSLContextBuilder();
+            KeyStore trustStore = KeyStore.getInstance(KeyStore.getDefaultType());
+            //信任任何链接
+            TrustStrategy anyTrustStrategy = new TrustStrategy() {
+                @Override
+                public boolean isTrusted(X509Certificate[] x509Certificates, String s) throws CertificateException {
+                    return true;
+                }
+            };
+            SSLContext sslContext = SSLContexts.custom().useProtocol("TLS").loadTrustMaterial(trustStore, anyTrustStrategy).build();
+            LayeredConnectionSocketFactory sslsf = new SSLConnectionSocketFactory(sslContext, NoopHostnameVerifier.INSTANCE);
+
+            /*SSLContextBuilder builder = new SSLContextBuilder();
             builder.loadTrustMaterial(null, new TrustSelfSignedStrategy());
-            SSLConnectionSocketFactory sslsf = new SSLConnectionSocketFactory(
-                    builder.build());
+            SSLConnectionSocketFactory sslsf = new SSLConnectionSocketFactory(builder.build());*/
             // 配置同时支持 HTTP 和 HTPPS
             Registry<ConnectionSocketFactory> socketFactoryRegistry = RegistryBuilder.<ConnectionSocketFactory> create().register(
                     "http", PlainConnectionSocketFactory.getSocketFactory()).register(
                     "https", sslsf).build();
             // 初始化连接管理器
-            pool = new PoolingHttpClientConnectionManager(
-                    socketFactoryRegistry);
-            // 将最大连接数增加到200，实际项目最好从配置文件中读取这个值
+            pool = new PoolingHttpClientConnectionManager(socketFactoryRegistry);
+            // 将最大连接数增加到200，实际项目最好从配置文件中读取这个值，表示一次请求时间内能接收的最大请求数,整个池子的大小
             pool.setMaxTotal(200);
-            // 设置最大路由
-            pool.setDefaultMaxPerRoute(2);
+            // 设置最大路由，服务每次能并行接收的请求数
+            pool.setDefaultMaxPerRoute(50);
             // 根据默认超时限制初始化requestConfig
             int socketTimeout = 10000;
             int connectTimeout = 10000;
@@ -87,7 +103,15 @@ public class HttpClientUtil {
             requestConfig = RequestConfig.custom().setConnectionRequestTimeout(
                     connectionRequestTimeout).setSocketTimeout(socketTimeout).setConnectTimeout(
                     connectTimeout).build();
-
+            //http client初始化
+            httpClient = HttpClients.custom()
+                    // 设置连接池管理
+                    .setConnectionManager(pool)
+                    // 设置请求配置
+                    .setDefaultRequestConfig(requestConfig)
+                    // 设置重试次数
+                    .setRetryHandler(new DefaultHttpRequestRetryHandler(0, false))
+                    .build();
             //System.out.println("初始化HttpClientTest~~~结束");
         } catch (NoSuchAlgorithmException e) {
             e.printStackTrace();
@@ -101,19 +125,6 @@ public class HttpClientUtil {
                 .setConnectionRequestTimeout(50000).build();*/
     }
 
-    public static CloseableHttpClient getHttpClient() {
-        
-        CloseableHttpClient httpClient = HttpClients.custom()
-                // 设置连接池管理
-                .setConnectionManager(pool)
-                // 设置请求配置
-                .setDefaultRequestConfig(requestConfig)
-                // 设置重试次数
-                .setRetryHandler(new DefaultHttpRequestRetryHandler(0, false))
-                .build();
-        
-        return httpClient;
-    }
 
     /**
      * 发送Post请求
@@ -121,16 +132,13 @@ public class HttpClientUtil {
      * @return
      */
     private static String sendHttpPost(HttpPost httpPost) {
-
-        CloseableHttpClient httpClient = null;
+        //CloseableHttpClient client = null;
         CloseableHttpResponse response = null;
         // 响应内容
         String responseContent = null;
         try {
-            // 创建默认的httpClient实例.
-            httpClient = getHttpClient();
             // 配置请求信息
-            httpPost.setConfig(requestConfig);
+            //httpPost.setConfig(requestConfig);
             //添加请求头信息，getHeader()可以获得请求头Request Header
             //httpPost.setHeader("Cookie", "JSESSIONID=A8E1B5509812A6B7C32F9AA9EE31FBF0");
             // 执行请求
@@ -152,8 +160,7 @@ public class HttpClientUtil {
 
             // 判断响应状态
             if (response.getStatusLine().getStatusCode() >= 300) {
-                throw new Exception(
-                        "HTTP Request is not success, Response code is " + response.getStatusLine().getStatusCode());
+                throw new Exception("HTTP Request is not success, Response code is " + response.getStatusLine().getStatusCode());
             }
 
             if (HttpStatus.SC_OK == response.getStatusLine().getStatusCode()) {
@@ -182,16 +189,13 @@ public class HttpClientUtil {
      * @return
      */
     private static String sendHttpGet(HttpGet httpGet) {
-
-        CloseableHttpClient httpClient = null;
+        //CloseableHttpClient client = null;
         CloseableHttpResponse response = null;
         // 响应内容
         String responseContent = null;
         try {
-            // 创建默认的httpClient实例.
-            httpClient = getHttpClient();
             // 配置请求信息
-            httpGet.setConfig(requestConfig);
+            //httpGet.setConfig(requestConfig);
             // 执行请求
             response = httpClient.execute(httpGet);
             // 得到响应实例
@@ -379,14 +383,20 @@ public class HttpClientUtil {
         return parameterBuilder.toString();
     }
 
+
     public static void main(String[] args) throws Exception {
         
         //System.out.println(sendHttpGet("http://www.baidu.com"));
     	//String httpUrl = "http://192.168.0.2:8080/nutritionV2/user/findUserByUserId;jsessionid=A8E1B5509812A6B7C32F9AA9EE31FBF0";
-    	String httpUrl = "http://192.168.0.2:8080/nutritionV2/user/findUserByUserId";
+    	/*String httpUrl = "http://192.168.0.2:8080/nutritionV2/user/findUserByUserId";
     	Map<String, Object> maps = new HashMap<>();
     	maps.put("userId", 11908);
     	maps.put("hospitalId", 42);
-    	System.out.println(sendHttpPost(httpUrl, maps));
+    	System.out.println(sendHttpPost(httpUrl, maps));*/
+
+        //String url = "http://report.91160.com/report";
+        String url = "https://218.17.222.229/com/installClient.html";
+        String str = sendHttpGet(url);
+        System.out.println(str);
     }
 }
